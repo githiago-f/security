7ce87dc2f64bc5458e40935dbe27066095258500
githiago-f <tfarias@protonmail.com>
2022-02-18 20:16:52 -0300

    refactor: use global registry sdk

--- apps/residents/src/app/app.module.ts
+++ apps/residents/src/app/app.module.ts
@@ -1,29 +1,28 @@
 import { Module, OnApplicationBootstrap } from '@nestjs/common';
-import { DwellingsController } from './event/dwellings/dwellings.controller';
-import { DwellingsModule } from '@dwellings/modules/dwellings/dwellings.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { registryConfig } from 'apps/shared/config/registry';
+import { ResidentsController } from './event/residents/residents.controller';
+import { ResidentsModule } from '@residents/modules/residents/residents.module';
 import { ServiceConfig } from '@shared/types/service-config';
-import { serviceConfig } from './config/service';
+import { serviceConfig } from '@dwellings/app/config/service';
 import { registerService } from '@shared/sdk/register-service';
 
 @Module({
   imports: [
-    DwellingsModule,
+    ResidentsModule,
     ConfigModule.forRoot({
       envFilePath: [ 'apps/dwellings/.env' ],
       load: [registryConfig, serviceConfig],
     }),
   ],
-  controllers: [DwellingsController],
+  controllers: [ResidentsController],
 })
 export class AppModule implements OnApplicationBootstrap {
-  constructor(private readonly config: ConfigService) {}
+  constructor(private config: ConfigService) {}
 
   async onApplicationBootstrap() {
-    const registryHost = this.config.get('registry.service');
-    const service = this.config.get<ServiceConfig>('dwellings');
-    await registerService(registryHost, service);
+    const registryUrl = this.config.get('registry.service');
+    const service = this.config.get<ServiceConfig>('residents');
+    await registerService(registryUrl, service);
   }
-
 }
--- apps/residents/src/app/config/service.ts
+++ apps/residents/src/app/config/service.ts
@@ -1,11 +1,11 @@
 import { ServiceConfig } from '@shared/types/service-config';
 
 export type Config = {
-  dwellings: ServiceConfig,
+  residents: ServiceConfig,
 };
 
 export const serviceConfig = (): Config => ({
-  dwellings: {
+  residents: {
     port: +process.env.PORT,
     hostname: process.env.HOSTNAME,
     service: process.env.APP_NAME,
